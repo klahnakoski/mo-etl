bd7582a98350bbc896a0bb5a138547f68ad3437b
Kyle Lahnakoski <kyle@lahnakoski.com>
2020-03-21 19:28:49 -0400

    more code!

--- docs/etl_schedulers.md
+++ docs/etl_schedulers.md
@@ -1,31 +1,69 @@
 ## Counting caught backouts
 
-* Never run
-* run but failed
-* run and passed
-* run and intermittent
-
-* before backout
-* after backout and before backout_by
-* after backout_by
 
 
 
-### Chart of single job, and if it can be blamed for a regression
+
+Chart of single job, and if it can be blamed for a regression
 
 * **none** - Never run
 * **fail** - always fails
 * **pass** - always passed
 * **inter** - intermittent
 
+Deeper analysis:  For any test/manifest/job we can view its behaviour during three distinct periods
 
-* Rows: before the backed out
-* Columns: After the backouted out
-* `X` possible cause 
+* **before** - how the test was behaving before the bad push
+* **during** - how the test was behaving between the bad push and the execution of the backout
+* **after** - how the test was behaving after backout was executed
+
+For example, a test that was passing before the bad push, failing after the bad push, and then passing after the backout will be considered a good test to catch that bad push.  Here is the full truth table:
+
+
+
+| before | during | after | blame | 
+|--------|--------|-------|-------|
+| none   |  none  | none  |       |
+| fail   |  none  | none  |       |
+| pass   |  none  | none  |       |
+| inter  |  none  | none  |       |
+| none   |  fail  | none  |       |
+| fail   |  fail  | none  |       |
+| pass   |  fail  | none  |   X   |
+| inter  |  fail  | none  |       |
+| none   |  pass  | none  |       |
+| fail   |  pass  | none  |       |
+| pass   |  pass  | none  |       |
+| inter  |  pass  | none  |       |
+| none   |  inter | none  |       |
+| fail   |  inter | none  |       |
+| pass   |  inter | none  |       |
+| inter  |  inter | none  |       |
+
+
+
+
+
+Simple case
 
 |       | none | fail | pass | inter | 
 |-------|------|------|------|-------|
 | none  |      |      |      |       | 
-| fail  |      |      |      |       |
 | pass  |      |  `X` |      |  `X`  |
 | inter |      |  `X` |      |       |
+| fail  |      |      |      |       |
+
+
+Where
+* Rows: before the backed out
+* Columns: After the backouted out
+* `X` possible cause 
+
+
+
+
+
+* before backout
+* after backout and before backout_by
+* after backout_by
+
--- mo_etl/etl-schedulers.py
+++ mo_etl/etl-schedulers.py
@@ -9,6 +9,8 @@
 from __future__ import division
 from __future__ import unicode_literals
 
+from copy import copy
+
 import loguru
 
 import adr
@@ -18,26 +20,31 @@ from adr.errors import MissingDataError
 from jx_bigquery import bigquery
 from jx_python import jx
 from mo_dots import Data, coalesce, listwrap, wrap
-from mo_future import first
 from mo_logs import startup, constants, Log, machine_metadata
-from mo_threads import Queue, Thread, MAIN_THREAD
+from mo_threads import Queue, Thread, MAIN_THREAD, THREAD_STOP, Lock
 from mo_times import Date, Duration
 from mozci.push import make_push_objects
 from mozci.task import Status
 from pyLibrary.env import git
 
+LOOK_BACK = 30
+LOOK_FORWARD = 30
 
-def process(config, please_stop):
+
+def normalize_config(config):
     config.range.min = Date(config.range.min)
     config.range.max = Date(config.range.max)
     config.start = Date(config.start)
     config.interval = Duration(config.interval)
     config.branches = listwrap(config.branches)
     config.schedulers = listwrap(config.schedulers)
-    destination = bigquery.Dataset(config.destination).get_or_create_table(
+    config._destination = bigquery.Dataset(config.destination).get_or_create_table(
         config.destination
     )
+    return config
 
+
+def process(config, please_stop):
     todo = Queue("work", max=10000)
 
     etl_config_table = jx_sqlite.Container(config.config_db).get_or_create_facts(
@@ -105,9 +112,15 @@ def process(config, please_stop):
                 {
                     "name": name,
                     "child_count": child_count,
-                    "status": {Status.PASS: "pass", Status.FAIL: "fail", Status.INTERMITTENT: "intermittent"}[status]
+                    "status": {
+                        Status.PASS: "pass",
+                        Status.FAIL: "fail",
+                        Status.INTERMITTENT: "intermittent",
+                    }[status],
                 }
-                for name, (child_count, status) in push.get_candidate_regressions("label").items()
+                for name, (child_count, status) in push.get_candidate_regressions(
+                    "label"
+                ).items()
             ]
             backout_type = None
             if backout_by:
@@ -134,7 +147,7 @@ def process(config, please_stop):
                 }
             )
         Log.note("adding {{num}} records to bigquery", num=len(data))
-        destination.extend(data)
+        config._destination.extend(data)
 
     try:
         while not please_stop:
@@ -147,7 +160,7 @@ def process(config, please_stop):
     except Exception as e:
         Log.warning("Could not complete the etl", cause=e)
 
-    destination.merge_shards()
+    config._destination.merge_shards()
 
 
 def main():
@@ -161,12 +174,11 @@ def main():
         # https://loguru.readthedocs.io/en/stable/api/logger.html#loguru._logger.Logger.add
         loguru.logger.remove()
         loguru.logger.add(
-            _logging,
-            level="DEBUG",
-            format="{message}",
-            filter=lambda r: True,
+            _logging, level="DEBUG", format="{message}", filter=lambda r: True,
         )
         Log.start(config.debug)
+        config = normalize_config(config)
+
         Thread.run("processing", process, config)
         MAIN_THREAD.wait_for_shutdown_signal(wait_forever=False)
     except Exception as e:
@@ -175,13 +187,201 @@ def main():
         Log.stop()
 
 
+def all_pushes(config):
+    todo = Queue("work", max=10000)
+
+    etl_config_table = jx_sqlite.Container(config.config_db).get_or_create_facts(
+        "etl-range"
+    )
+    data = wrap(etl_config_table.query()).data
+    prev_done = data[0]
+    done = Data(
+        min=Date(coalesce(prev_done.min, config.start, "today-2day")),
+        max=Date(coalesce(prev_done.max, config.start, "today-2day")),
+    )
+    if not len(data):
+        etl_config_table.add(done)
+
+    if done.max < config.range.max:
+        # ADD WORK GOING FORWARDS
+        start = Date.floor(done.max, config.interval)
+        while start < config.range.max:
+            end = start + config.interval
+            for branch in config.branches:
+                for sched in config.schedulers:
+                    todo.add((start, end, branch, sched))
+            start = end
+    if config.range.min < done.min:
+        # ADD WORK GOING BACKWARDS
+        end = Date.ceiling(done.min, config.interval)
+        while config.range.min < end:
+            start = end - config.interval
+            for branch in config.branches:
+                for sched in config.schedulers:
+                    todo.add((start, end, branch, sched))
+            end = start
+
+
+    def process_one(start, end, branch, scheduler):
+        # ASSUME PREVIOUS WORK IS DONE
+        # UPDATE THE DATABASE STATE
+        done.min = mo_math.min(end, done.min)
+        done.max = mo_math.max(start, done.max)
+        etl_config_table.update({"set": done})
+
+        # compute dates in range
+        try:
+            pushes = make_push_objects(
+                from_date=start.format(), to_date=end.format(), branch=branch
+            )
+        except MissingDataError:
+            pushes = []
+        except Exception as e:
+            raise Log.error("not expected", cause=e)
+
+        Log.note(
+            "Found {{num}} pushes for {{scheduler}} on {{branch}} in ({{start}}, {{end}})",
+            num=len(pushes),
+            start=start,
+            end=end,
+            scheduler=scheduler,
+            branch=branch,
+        )
+        data = []
+        for push in pushes:
+            tasks = push.get_shadow_scheduler_tasks(scheduler)
+            likely_regressions = push.get_likely_regressions("label")
+            backout_by = push.backedoutby()
+            candidate_regressions = [
+                {
+                    "name": name,
+                    "child_count": child_count,
+                    "status": {
+                        Status.PASS: "pass",
+                        Status.FAIL: "fail",
+                        Status.INTERMITTENT: "intermittent",
+                    }[status],
+                }
+                for name, (child_count, status) in push.get_candidate_regressions(
+                    "label"
+                ).items()
+            ]
+            backout_type = None
+            if backout_by:
+                if likely_regressions & tasks:
+                    backout_type = "primary"
+                else:
+                    backout_type = "secondary"
+
+            data.append(
+                {
+                    "push": {
+                        "id": push.id,
+                        "date": push.date,
+                        "changesets": push.revs,
+                    },
+                    "tasks": jx.sort(tasks),
+                    "likely_regressions": jx.sort(likely_regressions),
+                    "candidate_regressions": jx.sort(candidate_regressions, "name"),
+                    "scheduler": scheduler,
+                    "branch": branch,
+                    "backout_type": backout_type,
+                    "backout_by": backout_by,
+                    "etl": {"version": git.get_revision(), "timestamp": Date.now()},
+                }
+            )
+        Log.note("adding {{num}} records to bigquery", num=len(data))
+        config._destination.extend(data)
+
+    try:
+        while not please_stop:
+            try:
+                start, end, branch, scheduler = todo.pop_one()
+            except Exception:
+                Log.note("no more work")
+                break
+            process_one(start, end, branch, scheduler)
+    except Exception as e:
+        Log.warning("Could not complete the etl", cause=e)
+
+    config._destination.merge_shards()
+
+
+
+
+
+
+    state = Data(locker=Lock(), labels={})
+    backedout = Queue("backouts")
+
+    def get_parents(from_date, start_push, please_stop):
+        # ITERATE BACKWARDS, TRIGGERING LOCAL CACHE
+        push = start_push
+        i = 0
+        while not please_stop and Date(push.date) > from_date:
+            push.id
+            push.revs
+            push.backsoutnodes
+            with state.locker:
+                state.labels |= push.label_summaries().keys()
+                for name, summary in push.label_summaries().items():
+                    state.results[name][push.id][summary.status.name] += 1
+            if push.backedoutby and i > LOOK_FORWARD:
+                backedout.add(push)
+
+        backedout.add(THREAD_STOP)
+
+    worker = Thread.run("get_pushes", get_parents)
+
+    # GET TASKS AND TASK RESULTS FOR PUSH
+    # FOR EACH BACKED OUT PUSH
+    # BETWEEN BACKEDOUT AND BACKOUT DETERMINE TASK STATUS
+    while True:
+        bad = backedout.pop()
+        # ENSURE WE HAVE THE PUSH SEQUENCE COVERING (start, bad, backout, end)
+        # sequence = before + during + after
+        parts = Data()
+        start = bad
+        for i in range(LOOK_BACK):
+            start = start.parent
+            parts.before += [start.id]
+        backout = bad.backedoutby
+        end = bad
+        while end.id < backout.id:
+            parts.during += [end.id]
+            end = end.child
+        for i in range(LOOK_FORWARD):
+            parts.after += [end.id]
+            end = end.child
+
+        with state.locker:
+            labels = copy(state.labels)
+
+        indicators = []
+        for label in jx.sort(labels):
+            pushes = state.results[label]
+
+            about = Data()
+            for p in ("before", "during", "after"):
+                for s in Status:
+                    about[p][s.name] = sum(pushes[i][s.name] for i in parts[p])
+
+            # IS THIS LABEL AN INDICATOR?
+            if (
+                coalesce(about.before.PASS, 0) > coalesce(about.before.FAIL, 0)  # SUCCESS BEFORE BAD PUSH
+                and coalesce(about.during.PASS, 0) < coalesce(about.during.FAIL, 0)  # FAILURE DURING BAD PUSH
+                and coalesce(about.after.PASS, 0) > coalesce(about.after.FAIL, 0)  # SUCCESS AFTER BACKOUT
+            ):
+                indicators.add(label)
+
+
 def _logging(message):
     # params = wrap(message.record)
     # Log.note(message, default_params=params)
 
     params = message.record
-    params['machine'] = machine_metadata
-    log_format = "{{machine.name}} (pid {{process}}) - {{time|datetime}} - {{thread}} - \"{{file}}:{{line}}\" - ({{function}}) - {{message}}"
+    params["machine"] = machine_metadata
+    log_format = '{{machine.name}} (pid {{process}}) - {{time|datetime}} - {{thread}} - "{{file}}:{{line}}" - ({{function}}) - {{message}}'
     Log.main_log.write(log_format, params)
 
 
